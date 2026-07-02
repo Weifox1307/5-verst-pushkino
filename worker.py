6efa73f56b01eee9566e2e62734c057eae49bc5d
Roman Bekov <bekovroman0@gmail.com>
2026-07-02 12:10:52 +0300

    Update worker.py

--- worker.py
+++ worker.py
@@ -6,7 +6,7 @@ from datetime import timedelta, timezone
 NRMS_USER = os.getenv("NRMS_USERNAME")
 NRMS_PASS = os.getenv("NRMS_PASSWORD")
 SHEET_URL = os.getenv("SHEET_CSV_URL")
-EVENT_ID = 10061  # Станкозавод
+EVENT_ID = 10013  # Станкозавод
 
 def get_moscow_now():
     """Возвращает текущее время в Москве (UTC+3)"""
